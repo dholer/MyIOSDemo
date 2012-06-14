09ce0706405568a2a880e058367e24a5fe66ffcb
joseph <fangtee@qq.com>
2012-06-14 23:51:22 +0800

    first commit

--- MyIOSDemo/dholerAppDelegate.h
+++ MyIOSDemo/dholerAppDelegate.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+@class dholerViewController;
+
 @interface dholerAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) dholerViewController *viewController;
 
 @end
--- MyIOSDemo/dholerAppDelegate.m
+++ MyIOSDemo/dholerAppDelegate.m
@@ -8,16 +8,34 @@
 
 #import "dholerAppDelegate.h"
 
+#import "dholerViewController.h"
+
 @implementation dholerAppDelegate
 
 @synthesize window = _window;
+@synthesize viewController = _viewController;
+
+- (void)dealloc
+{
+    [_window release];
+    [_viewController release];
+    [super dealloc];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        self.viewController = [[[dholerViewController alloc] initWithNibName:@"dholerViewController_iPhone" bundle:nil] autorelease];
+    } else {
+        self.viewController = [[[dholerViewController alloc] initWithNibName:@"dholerViewController_iPad" bundle:nil] autorelease];
+    }
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
